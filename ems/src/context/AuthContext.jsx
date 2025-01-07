ecebee7497f9ae8eb1bb98d0061dba72a90074a1
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-01-07 11:57:16 +0530

    Added TaskList, Dashboard , Auth,Pages, context, utils folder in project and also install tailwind css for react vite

---
+++ ems/src/context/AuthContext.jsx
@@ -0,0 +1,10 @@
+// In this file there authorization and already prepared user names
+import React from 'react'
+
+const AuthContext = () => {
+  return (
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
